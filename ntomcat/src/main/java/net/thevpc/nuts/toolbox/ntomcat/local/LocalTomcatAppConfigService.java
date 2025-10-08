e93f8128f919d517014d2ae2b086d59e5135b683
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:40 +0100

    Update LocalTomcatAppConfigService.java : refactor NMsg

--- ntomcat/src/main/java/net/thevpc/nuts/toolbox/ntomcat/local/LocalTomcatAppConfigService.java
+++ ntomcat/src/main/java/net/thevpc/nuts/toolbox/ntomcat/local/LocalTomcatAppConfigService.java
@@ -12,7 +12,7 @@ import net.thevpc.nuts.toolbox.ntomcat.NTomcatConfigVersions;
 import net.thevpc.nuts.toolbox.ntomcat.local.config.LocalTomcatAppConfig;
 import net.thevpc.nuts.toolbox.ntomcat.util._StringUtils;
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
